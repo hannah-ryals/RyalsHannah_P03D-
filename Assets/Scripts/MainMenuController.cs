6e2c216267318ab25c208e6a205b84b317096829
Hannah Ryals <hannah.ryals888@gmail.com>
2021-05-05 01:42:47 -0500

    Completed MainMenu for the most part

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -6,31 +6,18 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] AudioClip _startingSong;
-    [SerializeField] Text _highScoreTextView;
+    
 
 
     void Start()
     {
-        int highScore = PlayerPrefs.GetInt("HighScore");
-        _highScoreTextView.text = highScore.ToString();
-
+       
         if(_startingSong != null)
         {
             AudioManager.Instance.PlaySong(_startingSong);
         }
         
     }
-
-    public void ResetHighScore()
-    {
-        PlayerPrefs.SetInt("HighScore", 0);
-        _highScoreTextView.text = PlayerPrefs.GetInt("HighScore").ToString();
-    }
-
-   /* public void HideMouse()
-    {
-        Cursor.visible = false;
-    } */
 
     public void EndGame()
     {
