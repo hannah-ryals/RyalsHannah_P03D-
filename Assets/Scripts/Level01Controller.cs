80471af90daa20ca21fa18497b0874ec356c98f9
Hannah Ryals <hannah.ryals888@gmail.com>
2021-05-05 01:54:22 -0500

    Made 4 viewports
    May add more, depends on level design

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -7,20 +7,15 @@ using UnityEngine.UI;
 public class Level01Controller : MonoBehaviour
 {
     [SerializeField] Text _currentScoreTextView;
-    public Text timerText;
-    public Text timeIsOut;
     public GameObject player;
     public AudioSource intro;
-    public AudioSource end;
 
     int _currentScore;
 
     void Start()
     {
-        StartCoroutine(reloadTimer(0));
-        StartCoroutine(EndTimer(0));
-        intro.Play();
-        
+        //intro.Play();
+
     }
 
     private void Update()
@@ -32,69 +27,20 @@ public class Level01Controller : MonoBehaviour
         }
     }
 
-    public void IncreaseScore(int scoreIncrease)
+
+    public void IncreaseScore()
     {
-        _currentScore += scoreIncrease;
+        _currentScore += 1;
         _currentScoreTextView.text = "Score: " + _currentScore.ToString();
     }
 
-   
+
 
     public void ExitLevel()
     {
         SceneManager.LoadScene("MainMenu");
-        //Cursor.visible = true;
-        int highScore = PlayerPrefs.GetInt("HighScore");
-        if (_currentScore > highScore)
-        {
-            PlayerPrefs.SetInt("HighScore", _currentScore);
-            Debug.Log("New high score: " + _currentScore);
-            
-        }
-        SceneManager.LoadScene("MainMenu");
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
 
-    }
-
-  
-
-    IEnumerator reloadTimer(float reloadTimeInSeconds)
-    {
-        float counter = -30;
-
-        while (counter < reloadTimeInSeconds)
-        {
-            counter += Time.deltaTime;
-            timerText.text = counter.ToString();
-            yield return null;
-        }
-
-        if (counter >= 0)
-        {
-            end.Play();
-            counter = 0;
-            timerText.text = counter.ToString();
-            timeIsOut.transform.localScale = new Vector3(1, 1, 1);
-            
-            //Transport();
-            Debug.Log("time ran out");
-        }
-    }
-
-    IEnumerator EndTimer(float EndTimerInSeconds)
-    {
-        float counterdos = -35;
-
-        while (counterdos < EndTimerInSeconds)
-        {
-            counterdos += Time.deltaTime;
-            yield return null;
-        }
-
-        if (counterdos >= 0)
-        {
-            ExitLevel();
-        }
     }
 }
